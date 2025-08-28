eb081946cbc82460bede6d216df5cecb5d99d8f5
Golubtsov Prokop <golubtsovpyu@intech.rshb.ru>
2025-08-28 21:53:17 +0300

    init structure

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './index.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
